21c57e10f76cd652307459d9f66147e6b01a0d02
Armando Herrera <anhydrous99@gmail.com>
2020-01-30 12:40:16 -0600

    Using multiprocessing run several experiments at once

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
+import ray
 import json
 import argparse
 from train import train
 
 
 def main():
-
-
     parser = argparse.ArgumentParser(description='Deep Q-network (DQN)')
     parser.add_argument('--conf', type=str, default='conf.json')
     args = parser.parse_args()
@@ -23,24 +22,25 @@ def main():
     epsilon_decay_last_frame = conf_json['epsilon_decay_last_frame']
     epsilon_start = conf_json['epsilon_start']
     epsilon_final = conf_json['epsilon_final']
-
     runs = conf_json['runs']
+    ray.init()
 
-    for run in runs:
-        env_str = run['env']
-
-        ngamma = run['gamma'] if 'gamma' in run else gamma
-        nbatch_size = run['batch_size'] if 'batch_size' in run else batch_size
-        nreplay_size = run['replay_size'] if 'replay_size' in run else replay_size
-        nlearning_rate = run['learning_rate'] if 'learning_rate' in run else learning_rate
-        nsync_target_frames = run['sync_target_frames'] if 'sync_target_frames' in run else sync_target_frames
-        nreplay_start_size = run['replay_start_size'] if 'replay_start_size' in run else replay_start_size
-        nepsilon_decay_last_frame = run['epsilon_decay_last_frame'] if 'epsilon_decay_last_frame' in run else epsilon_decay_last_frame
-        nepsilon_start = run['epsilon_start'] if 'epsilon_start' in run else epsilon_start
-        nepsilon_final = run['epsilon_final'] if 'epsilon_final' in run else epsilon_final
-
-        if 'train_reward' in run:
-            train_reward = run['train_reward']
+    @ray.remote(num_cpus=2, num_gpus=0.2)
+    def fun(frun, gamma, batch_size, replay_size, learning_rate, sync_target_frames, replay_start_size,
+            epsilon_decay_last_frame, epsilon_start, epsilon_final):
+        env_str = frun['env']
+        ngamma = frun['gamma'] if 'gamma' in frun else gamma
+        nbatch_size = frun['batch_size'] if 'batch_size' in frun else batch_size
+        nreplay_size = frun['replay_size'] if 'replay_size' in frun else replay_size
+        nlearning_rate = frun['learning_rate'] if 'learning_rate' in frun else learning_rate
+        nsync_target_frames = frun['sync_target_frames'] if 'sync_target_frames' in frun else sync_target_frames
+        nreplay_start_size = frun['replay_start_size'] if 'replay_start_size' in frun else replay_start_size
+        nepsilon_decay_last_frame = frun[
+            'epsilon_decay_last_frame'] if 'epsilon_decay_last_frame' in frun else epsilon_decay_last_frame
+        nepsilon_start = frun['epsilon_start'] if 'epsilon_start' in frun else epsilon_start
+        nepsilon_final = frun['epsilon_final'] if 'epsilon_final' in frun else epsilon_final
+        if 'train_reward' in frun:
+            train_reward = frun['train_reward']
             train(env_str,
                   ngamma,
                   nbatch_size,
@@ -53,8 +53,8 @@ def main():
                   nepsilon_final,
                   None,
                   train_reward)
-        elif 'train_frames' in run:
-            train_frames = run['train_frames']
+        elif 'train_frames' in frun:
+            train_frames = frun['train_frames']
             train(env_str,
                   ngamma,
                   nbatch_size,
@@ -67,6 +67,16 @@ def main():
                   nepsilon_final,
                   train_frames,
                   None)
+        return 1
+
+    remote_objects = []
+    for run in runs:
+        robj = fun.remote(run, gamma, batch_size, replay_size, learning_rate, sync_target_frames, replay_start_size,
+                          epsilon_decay_last_frame, epsilon_start, epsilon_final)
+        remote_objects.append(robj)
+    values = ray.get(remote_objects)
+    for val in values:
+        assert val == 1
 
 
 if __name__ == '__main__':
