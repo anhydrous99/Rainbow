518b4d04bcbe9c03de5083d2644ed8ce5e0656da
Armando Herrera <anhydrous99@gmail.com>
2020-01-30 19:15:12 -0600

    A couple fixes

--- run.py
+++ run.py
@@ -30,45 +30,45 @@ def main():
 
     @conditional_decorator(ray.remote(num_cpus=conf_json['num_cpus'], num_gpus=conf_json['num_gpus']),
                            conf_json['multiprocessing'])
-    def fun(f_run, gamma, batch_size, replay_size, learning_rate, sync_target_frames, replay_start_size,
-            epsilon_decay_last_frame, epsilon_start, epsilon_final):
+    def fun(f_run, f_gamma, f_batch_size, f_replay_size, f_learning_rate, f_sync_target_frames, f_replay_start_size,
+            f_epsilon_decay_last_frame, f_epsilon_start, f_epsilon_final):
         env_str = f_run['env']
-        ngamma = f_run['gamma'] if 'gamma' in f_run else gamma
-        nbatch_size = f_run['batch_size'] if 'batch_size' in f_run else batch_size
-        nreplay_size = f_run['replay_size'] if 'replay_size' in f_run else replay_size
-        nlearning_rate = f_run['learning_rate'] if 'learning_rate' in f_run else learning_rate
-        nsync_target_frames = f_run['sync_target_frames'] if 'sync_target_frames' in f_run else sync_target_frames
-        nreplay_start_size = f_run['replay_start_size'] if 'replay_start_size' in f_run else replay_start_size
-        nepsilon_decay_last_frame = f_run[
-            'epsilon_decay_last_frame'] if 'epsilon_decay_last_frame' in f_run else epsilon_decay_last_frame
-        nepsilon_start = f_run['epsilon_start'] if 'epsilon_start' in f_run else epsilon_start
-        nepsilon_final = f_run['epsilon_final'] if 'epsilon_final' in f_run else epsilon_final
+        n_gamma = f_run['gamma'] if 'gamma' in f_run else f_gamma
+        n_batch_size = f_run['batch_size'] if 'batch_size' in f_run else f_batch_size
+        n_replay_size = f_run['replay_size'] if 'replay_size' in f_run else f_replay_size
+        n_learning_rate = f_run['learning_rate'] if 'learning_rate' in f_run else f_learning_rate
+        n_sync_target_frames = f_run['sync_target_frames'] if 'sync_target_frames' in f_run else f_sync_target_frames
+        n_replay_start_size = f_run['replay_start_size'] if 'replay_start_size' in f_run else f_replay_start_size
+        n_epsilon_decay_last_frame = f_run[
+            'epsilon_decay_last_frame'] if 'epsilon_decay_last_frame' in f_run else f_epsilon_decay_last_frame
+        n_epsilon_start = f_run['epsilon_start'] if 'epsilon_start' in f_run else f_epsilon_start
+        n_epsilon_final = f_run['epsilon_final'] if 'epsilon_final' in f_run else f_epsilon_final
         if 'train_reward' in f_run:
             train_reward = f_run['train_reward']
             train(env_str,
-                  ngamma,
-                  nbatch_size,
-                  nreplay_size,
-                  nreplay_start_size,
-                  nlearning_rate,
-                  nsync_target_frames,
-                  nepsilon_decay_last_frame,
-                  nepsilon_start,
-                  nepsilon_final,
+                  n_gamma,
+                  n_batch_size,
+                  n_replay_size,
+                  n_replay_start_size,
+                  n_learning_rate,
+                  n_sync_target_frames,
+                  n_epsilon_decay_last_frame,
+                  n_epsilon_start,
+                  n_epsilon_final,
                   None,
                   train_reward)
         elif 'train_frames' in f_run:
             train_frames = f_run['train_frames']
             train(env_str,
-                  ngamma,
-                  nbatch_size,
-                  nreplay_size,
-                  nreplay_start_size,
-                  nlearning_rate,
-                  nsync_target_frames,
-                  nepsilon_decay_last_frame,
-                  nepsilon_start,
-                  nepsilon_final,
+                  n_gamma,
+                  n_batch_size,
+                  n_replay_size,
+                  n_replay_start_size,
+                  n_learning_rate,
+                  n_sync_target_frames,
+                  n_epsilon_decay_last_frame,
+                  n_epsilon_start,
+                  n_epsilon_final,
                   train_frames,
                   None)
         return 1
@@ -76,16 +76,16 @@ def main():
     remote_objects = []
     for run in runs:
         if conf_json['multiprocessing']:
-            robj = fun.remote(run, gamma, batch_size, replay_size, learning_rate, sync_target_frames, replay_start_size,
-                              epsilon_decay_last_frame, epsilon_start, epsilon_final)
-            remote_objects.append(robj)
+            r_obj = fun.remote(run, gamma, batch_size, replay_size, learning_rate, sync_target_frames,
+                               replay_start_size, epsilon_decay_last_frame, epsilon_start, epsilon_final)
+            remote_objects.append(r_obj)
         else:
             fun(run, gamma, batch_size, replay_size, learning_rate, sync_target_frames, replay_start_size,
                 epsilon_decay_last_frame, epsilon_start, epsilon_final)
     if conf_json['multiprocessing']:
         values = ray.get(remote_objects)
-    for val in values:
-        assert val == 1
+        for val in values:
+            assert val == 1
 
 
 if __name__ == '__main__':
