874e653408df571412618410ff21a3f5dedf309d
Armando Herrera <anhydrous99@gmail.com>
2020-02-10 10:28:39 -0600

    fix bug

--- dqn_model.py
+++ dqn_model.py
@@ -5,7 +5,8 @@ from layers import NoisyDense, FactorizedNoisyDense
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) != 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    for device in physical_devices:
+        tf.config.experimental.set_memory_growth(device[0], True)
 
 
 def dense_chooser(dense=None):
