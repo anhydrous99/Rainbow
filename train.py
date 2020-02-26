59cbea6997bff69c11893170a377e4aaffdea401
Armando Herrera <anhydrous99@gmail.com>
2020-02-25 22:30:16 -0600

    better parameters

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ def train(env_name='PongNoFrameskip-v4',
         tf.random.set_seed(random_seed)
         env.seed(random_seed)
     f_name = env_name + "_" + run_name if run_name is not None else env_name
-    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, epsilon=1.5e-4)
     agent = Agent(env, replay_size, optimizer, batch_size, n_steps, gamma, use_double, use_dense, dueling,
                   use_categorical, n_atoms, v_min, v_max, train_frames if train_frames is not None else 5000000)
     if save_checkpoints:
